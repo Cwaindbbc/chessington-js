60b5a5634020c67a1238c1f3dc51a7a8c84e2e7d
Catherine Waind <catherine.waind@bbc.co.uk>
2020-07-13 11:30:15 +0100

    Added logic for getting available pawn moves

--- src/engine/pieces/pawn.js
+++ src/engine/pieces/pawn.js
@@ -1,11 +1,19 @@
-import Piece from './piece';
+import Piece from "./piece";
+import Player from "../player";
 
 export default class Pawn extends Piece {
-    constructor(player) {
-        super(player);
-    }
+  constructor(player) {
+    super(player);
+  }
 
-    getAvailableMoves(board) {
-        return new Array(0);
+  getAvailableMoves(board) {
+
+
+     if (this.player === Player.WHITE) {
+       return new Array({ row: 1, col: 0 });
+     }
+     if (this.player === Player.BLACK) {
+      return new Array({ row:6, col:7 });
     }
+  }
 }
