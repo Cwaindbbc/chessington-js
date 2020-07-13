8cec098ed8c3c3907cefdd60657fc3a158edbc88
Catherine Waind <catherine.waind@bbc.co.uk>
2020-07-13 16:55:03 +0100

    Pawn's ability to move forward 2 squares

--- src/engine/pieces/pawn.js
+++ src/engine/pieces/pawn.js
@@ -1,5 +1,6 @@
 import Piece from "./piece";
 import Player from "../player";
+import Square from "../square";
 
 export default class Pawn extends Piece {
   constructor(player) {
@@ -8,11 +9,35 @@ export default class Pawn extends Piece {
 
   getAvailableMoves(board) {
     const currentLocation = board.findPiece(this);
+    const possiblePawnLocations = [];
+
+    // If the pawn is on row 1, the can move 2 squares forward (or 1- players choice?)
+    // Does player want to move it one or two?
+    // can we move in to the plusOneRow somehow?
+
     if (this.player === Player.WHITE) {
-      currentLocation.row++;
+      const plusOneRow = currentLocation.row + 1;
+
+      const availableLocation = Square.at(plusOneRow, currentLocation.col);
+      possiblePawnLocations.push(availableLocation);
+
+      if (currentLocation.row === 1) {
+        const twoMovesRow = currentLocation.row + 2;
+
+        possiblePawnLocations.push(Square.at(twoMovesRow, currentLocation.col));
+      }
     } else {
-      currentLocation.row--;
+
+      if (currentLocation.row === 6) {
+        const twoMovesRow = currentLocation.row - 2;
+
+        possiblePawnLocations.push(Square.at(twoMovesRow, currentLocation.col));
+      }
+      const minusOneRow = currentLocation.row - 1;
+      const availableLocation = Square.at(minusOneRow, currentLocation.col);
+      possiblePawnLocations.push(availableLocation);
     }
-    return new Array(currentLocation);
+
+    return possiblePawnLocations;
   }
 }
